98b003b6d441af42f9e1ba5d8fc1c24d402fcdc4
Msbee502 <b.hoogesteyn@gmail.com>
2024-12-16 13:17:18 +0100

    Update About.jsx

--- myreactapp/src/pages/About.jsx
+++ myreactapp/src/pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div className="page">
       <div className="about">
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit sed
+          Lorem, biu iniunbipsum dolor sit amet consectetur adipisicing elit. Velit sed
           dignissimos architecto sun t molestias magnam? Nihil quaerat eos
           repudiandae harum dignissimos maiores placeat sit libero similique
           fugiat nesciunt at delectus neque blanditiis est quis quas, corrupti
